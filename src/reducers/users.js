fe0f8e2fa0be41a148f80a937243aac4d50b43e3
Kristy Chu <kristychu@kristys-mbp.lan>
2021-04-13 12:08:38 -0700

    update formatting of users.js reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,18 +1,18 @@
 const INITIAL_STATE = {
-    profile: {},
-    error: false
+  profile: {},
+  error: false,
 };
 
-export default function users(state=INITIAL_STATE, action){
-    switch (action.type) {
-        case 'LOAD_TOKEN':
-            return { ...state, profile: action.token }
-        case 'DELETE_TOKEN':
-            return { ...INITIAL_STATE }
-        case 'ERROR':
-            console.log(action.error)
-            return { ...state, error: true };
-        default:
-            return state;
-    }
+export default function users(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case "LOAD_TOKEN":
+      return { ...state, profile: action.token };
+    case "DELETE_TOKEN":
+      return { ...INITIAL_STATE };
+    case "ERROR":
+      console.log(action.error);
+      return { ...state, error: true };
+    default:
+      return state;
+  }
 }
