a7130f01d452ac8647e182fea708b655d8e5cd13
Kristy Chu <kristychu@kristys-mbp.lan>
2021-04-26 17:23:30 -0700

    take out test info and change history link to korean skincare routine page

--- src/Login.js
+++ src/Login.js
@@ -1,27 +1,27 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { useDispatch } from 'react-redux';
-import { login } from './actions';
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
-import './Login.css';
+import { useDispatch } from "react-redux";
+import { login } from "./actions";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import "./Login.css";
 
 function Login() {
   const dispatch = useDispatch();
-  const INITIAL_STATE = { username:"testuser", password:"password" };
+  const INITIAL_STATE = { username: "", password: "" };
   const [formData, setFormData] = useState(INITIAL_STATE);
   const history = useHistory();
 
-  const handleSubmit = evt => {
+  const handleSubmit = (evt) => {
     evt.preventDefault();
     dispatch(login(formData));
-    history.push('/user-routine')
+    history.push("/korean-skincare-routine");
   };
 
-  const handleChange = evt => {
-    const { name, value }= evt.target;
-    setFormData(fData => ({
-    ...fData,
-    [name]: value
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormData((fData) => ({
+      ...fData,
+      [name]: value,
     }));
   };
 
@@ -29,13 +29,27 @@ function Login() {
     <Form>
       <FormGroup>
         <Label for="username">Username</Label>
-        <Input type="username" name="username" id="username" value={formData.username} onChange={handleChange}/>
+        <Input
+          type="username"
+          name="username"
+          id="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
       </FormGroup>
       <FormGroup>
         <Label for="password">Password</Label>
-        <Input type="password" name="password" id="password" value={formData.password} onChange={handleChange}/>
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
       </FormGroup>
-      <Button color="primary" onClick={handleSubmit}>Submit</Button>
+      <Button color="primary" onClick={handleSubmit}>
+        Submit
+      </Button>
     </Form>
   );
 }
