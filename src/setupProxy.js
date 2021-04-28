52296c7c5c5f0e70cb8798d0843fd6fe0ae45447
Kristy Chu <kristychu@kristys-mbp.lan>
2021-04-27 22:20:40 -0700

    add /products endpoint to proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -9,6 +9,13 @@ module.exports = function (app) {
   );
 
   app.use(
+    proxy("/products", {
+      target: "https://skincare-api.herokuapp.com",
+      changeOrigin: true,
+    })
+  );
+
+  app.use(
     proxy("/api", {
       target: process.env.REACT_APP_BASE_URL || "http://localhost:3001",
       changeOrigin: true,
