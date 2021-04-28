44d19057c70d6b3ca2d4aa96bafc32084e3acddd
Kristy Chu <kristychu@kristys-mbp.lan>
2021-04-27 23:29:09 -0700

    add headers in setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,6 +4,7 @@ module.exports = function (app) {
   app.use(
     createProxyMiddleware("/product", {
       target: "https://skincare-api.herokuapp.com",
+      headers: { "Access-Control-Allow-Origin": "*" },
       changeOrigin: true,
     })
   );
@@ -11,6 +12,7 @@ module.exports = function (app) {
   app.use(
     createProxyMiddleware("/products", {
       target: "https://skincare-api.herokuapp.com",
+      headers: { "Access-Control-Allow-Origin": "*" },
       changeOrigin: true,
     })
   );
