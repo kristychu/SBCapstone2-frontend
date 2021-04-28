0485b5e33269e104282ff4d79a5fbd5be263eeab
Kristy Chu <kristychu@kristys-mbp.lan>
2021-04-27 23:16:04 -0700

    change proxy to explicit import

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,24 +1,22 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    proxy("/product", {
+    createProxyMiddleware("/product", {
       target: "https://skincare-api.herokuapp.com",
-      secure: false,
       changeOrigin: true,
     })
   );
 
   app.use(
-    proxy("/products", {
+    createProxyMiddleware("/products", {
       target: "https://skincare-api.herokuapp.com",
-      secure: false,
       changeOrigin: true,
     })
   );
 
   app.use(
-    proxy("/api", {
+    createProxyMiddleware("/api", {
       target: process.env.REACT_APP_BASE_URL || "http://localhost:3001",
       changeOrigin: true,
     })
